592ca1ecc5786b221c5cb44211995a868eeb5ce4
Duncan Gray <Duncan.Gray@ga.gov.au>
2018-09-03 15:35:33 +1000

    added comments

--- spatialhadoop_install.sh
+++ spatialhadoop_install.sh
@@ -5,4 +5,8 @@ git clone https://github.com/aseldawy/spatialhadoop2.git
 cd spatialhadoop2/
 mvn compile
 mvn package
+
+# To get the next command to work remove
+# testBuild111()
+# from edu.umn.cs.spatialHadoop.indexing.RStarTreeTest.test
 mvn assembly:assembly
