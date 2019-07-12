794b67be58ac375cdb88a1e6a611d4da954f31c7
Duncan Gray <duncan.gray@gmail.com>
2019-07-12 07:28:13 +0000

    fixes

--- deafrica_install.sh
+++ deafrica_install.sh
@@ -26,3 +26,5 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+
+echo "export EDITOR='/usr/bin/nano'" >> ~/.bashrc 
