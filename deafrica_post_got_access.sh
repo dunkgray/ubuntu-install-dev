d5b5b8472c29e6decc24696a85a7af170d9c112d
Duncan Gray <duncan.gray@gmail.com>
2019-07-17 01:27:01 +0000

    update

--- deafrica_post_got_access.sh
+++ deafrica_post_got_access.sh
@@ -1,3 +1,4 @@
 # let's get some repos
 git clone git@github.com:opendatacube/datacube-core.git ~/datacube-core
 git clone --branch dsg-dev git@github.com:GeoscienceAustralia/landsat-to-cog.git ~/landsat-to-cog
+git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git ~/cube-in-a-box
