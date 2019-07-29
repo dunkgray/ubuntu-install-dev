cf53568a096329a6d542fecb9cf4225e071fd227
Duncan Gray <duncan.gray@ga.gov.au>
2019-07-29 11:53:46 +1000

    bug fix

--- deafrica_pod.sh
+++ deafrica_pod.sh
@@ -4,7 +4,7 @@ apt-get -y install emacs
 pip3 install awscli
 
 # let's get some repos
-git clone https://github.com:opendatacube/datacube-core.git /opt/datacube-core
-git clone https://github.com:GeoscienceAustralia/landsat-to-cog.git /opt/landsat-to-cog
-git clone --branch usgs_l2_testing https://github.com:opendatacube/cube-in-a-box.git /opt/cube-in-a-box
-git clone --branch landsat_usgs_testing https://github.com:opendatacube/WOFLs-orchestration.git /opt/WOFLs-orchestration
+git clone https://github.com/opendatacube/datacube-core.git /opt/datacube-core
+git clone https://github.com/GeoscienceAustralia/landsat-to-cog.git /opt/landsat-to-cog
+git clone --branch usgs_l2_testing https://github.com/opendatacube/cube-in-a-box.git /opt/cube-in-a-box
+git clone --branch landsat_usgs_testing https://github.com/opendatacube/WOFLs-orchestration.git /opt/WOFLs-orchestration
