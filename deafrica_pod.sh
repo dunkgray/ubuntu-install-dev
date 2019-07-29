760ea9e7889e9b03080b4dd78d4ef41cf569370e
Duncan Gray <duncan.gray@ga.gov.au>
2019-07-29 12:36:38 +1000

    change branch

--- deafrica_pod.sh
+++ deafrica_pod.sh
@@ -10,4 +10,4 @@ git config --global user.name "Duncan Gray"
 git clone https://github.com/opendatacube/datacube-core.git /opt/datacube-core
 git clone https://github.com/GeoscienceAustralia/landsat-to-cog.git /opt/landsat-to-cog
 git clone --branch usgs_l2_testing https://github.com/opendatacube/cube-in-a-box.git /opt/cube-in-a-box
-git clone --branch landsat_usgs_testing https://github.com/opendatacube/WOFLs-orchestration.git /opt/WOFLs-orchestration
+git clone --branch dsg_landsat_usgs_testing https://github.com/opendatacube/WOFLs-orchestration.git /opt/WOFLs-orchestration
