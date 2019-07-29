cb14c32fc52f506c7d870ea3ebaabbf9dfa64d14
Duncan Gray <duncan.gray@ga.gov.au>
2019-07-29 11:48:12 +1000

    pod scripts but better

--- deafrica_pod.sh
+++ deafrica_pod.sh
@@ -2,5 +2,9 @@ apt-get update
 apt-get -y install postgresql
 apt-get -y install emacs
 pip3 install awscli
-chmod +x deafrica_post_got_access.sh
-.\deafrica_post_got_access.sh
+
+# let's get some repos
+git clone https://github.com:opendatacube/datacube-core.git /opt/datacube-core
+git clone https://github.com:GeoscienceAustralia/landsat-to-cog.git /opt/landsat-to-cog
+git clone --branch usgs_l2_testing https://github.com:opendatacube/cube-in-a-box.git /opt/cube-in-a-box
+git clone --branch landsat_usgs_testing https://github.com:opendatacube/WOFLs-orchestration.git /opt/WOFLs-orchestration
