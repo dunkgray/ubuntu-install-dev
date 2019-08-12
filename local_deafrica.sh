9614da6e2c71c0cfe202cc9d9fc395353abdbd50
Duncan Gray <duncan.gray@ga.gov.au>
2019-08-12 13:31:36 +1000

    new file

---
+++ local_deafrica.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git
+git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git
+git clone git@github.com:digitalearthafrica/config.git deafrica_config
+git clone git@github.com:dunkgray/DEAfrica_scripts.git DEAfrica_scripts
