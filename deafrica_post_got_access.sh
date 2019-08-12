a6a8dc82a73313142d2744c8939fa6c99f468eb1
Duncan Gray <duncan.gray@ga.gov.au>
2019-08-12 13:26:40 +1000

    small mod

--- deafrica_post_got_access.sh
+++ deafrica_post_got_access.sh
@@ -1,9 +1,10 @@
 # let's get some repos
 git clone git@github.com:opendatacube/datacube-core.git ~/datacube-core
 git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git ~/WOFLs-orchestration
-git clone git@github.com:dunkgray/DEAfrica_scripts.git ~/DEAfrica_scripts
 
-# Doing these remotely.  Show do this with everything really
-# git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git ~/landsat-to-cog
-# git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git ~/cube-in-a-box
-# git clone git@github.com:digitalearthafrica/config.git ~/deafrica_config
+
+# Doing these remotely.  Should do this with everything really
+# git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git
+# git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git
+# git clone git@github.com:digitalearthafrica/config.git deafrica_config
+# git clone git@github.com:dunkgray/DEAfrica_scripts.git DEAfrica_scripts
