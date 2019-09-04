9241124ae1e50fc2afc3f75dcc0aad0eb763f481
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-04 06:14:26 +0000

    file structure rearrange

---
+++ overall_ub.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Execute the initial start script
+./start_install.sh
+./ub_kubedocker.sh
+./conda_install.sh
+
+echo "copy secrets from .bashrc, .aws config and credentails from somewhere"
---
+++ ub_kubedocker.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Execute the initial start script
+./start_install.sh
+
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Install aws-iam-authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+
+
+# Docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo service docker start
+sudo usermod -aG docker $USER
+
+#aws access
+mkdir .aw
+
+
+# Testing
+# docker run hello-world
+
+echo "copy secrets from .bashrc, .aws config and credentails from somewhere"
