5537860c92853d5ebaa300519e65ef28df7aae93
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-10 01:51:10 -0400

    uncommenting and adding final dir

--- deafrica_post_got_access.sh
+++ deafrica_post_got_access.sh
@@ -1,14 +1,15 @@
 # let's get some repos
-git clone git@github.com:opendatacube/datacube-core.git ~/datacube-core
-git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git ~/WOFLs-orchestration
+SANDPIT=$HOME/sandpit
+git clone git@github.com:opendatacube/datacube-core.git $SANDPIT/datacube-core
+#git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git ~/WOFLs-orchestration
 
 
-# Doing these remotely.  Should do this with everything really
-# git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git
-# git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git
-# git clone git@github.com:digitalearthafrica/config.git deafrica_config
-# git clone git@github.com:dunkgray/DEAfrica_scripts.git DEAfrica_scripts
-# git clone --branch africa git@github.com:opendatacube/datacube-alchemist.git
-# git clone git@github.com:opendatacube/datacube-core.git
-#  git clone git@github.com:opendatacube/datacube-stats.git
-#  git clone git@github.com:GeoscienceAustralia/digitalearthau.git
+
+git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git  $SANDPIT/landsat-to-cog
+git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git  $SANDPIT/cube-in-a-box
+git clone git@github.com:digitalearthafrica/config.git $SANDPIT/config
+git clone git@github.com:dunkgray/DEAfrica_scripts.git $SANDPIT/DEAfrica_scripts
+git clone --branch africa git@github.com:opendatacube/datacube-alchemist.git  $SANDPIT/datacube-alchemist
+git clone git@github.com:opendatacube/datacube-core.git  $SANDPIT/datacube-core
+git clone git@github.com:opendatacube/datacube-stats.git  $SANDPIT/datacube-stats
+git clone git@github.com:GeoscienceAustralia/digitalearthau.git  $SANDPIT/digitalearthau
