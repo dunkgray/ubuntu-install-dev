809dd2e0383c19b0e314b1bbca2d32d30f3211fa
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-10 02:51:07 -0400

    modding for ubuntu laptop vm

--- dea_python.sh
+++ dea_python.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+SANDPIT=$HOME/sandpit
+
 # This is after pycharmremote has set up the repos....
-pip install --user -e pycharmremote/datacube-core/
+pip install --user -e $SANDPIT/datacube-core/
 # pyproject.toml doesn't work with the pip command
-rm pycharmremote/eo-datasets/pyproject.toml
-pip install --user -e pycharmremote/eo-datasets/
-pip install --user -e pycharmremote/datacube-alchemist
-pip install --user -e pycharmremote/digitalearthau #haven't done
-pip install --user -e pycharmremote/datacube-stats/
+rm $SANDPIT/eo-datasets/pyproject.toml
+pip install --user -e $SANDPIT/eo-datasets
+pip install --user -e $SANDPIT/datacube-alchemist
+pip install --user -e $SANDPIT/digitalearthau #haven't done
+pip install --user -e $SANDPIT/datacube-stats
--- deafrica_post_got_access.sh
+++ deafrica_post_got_access.sh
@@ -4,12 +4,16 @@ git clone git@github.com:opendatacube/datacube-core.git $SANDPIT/datacube-core
 #git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git ~/WOFLs-orchestration
 
 
-
-git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git  $SANDPIT/landsat-to-cog
+#
+#git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git  $SANDPIT/landsat-to-cog
+# git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git ~/WOFLs-orchestration
+git clone git@github.com:GeoscienceAustralia/eo-datasets.git $SANDPIT/eo-datasets
 git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git  $SANDPIT/cube-in-a-box
 git clone git@github.com:digitalearthafrica/config.git $SANDPIT/config
-git clone git@github.com:dunkgray/DEAfrica_scripts.git $SANDPIT/DEAfrica_scripts
+git clone git@github.com:digitalearthafrica/DEAfrica_scripts.git $SANDPIT/DEAfrica_scripts
 git clone --branch africa git@github.com:opendatacube/datacube-alchemist.git  $SANDPIT/datacube-alchemist
 git clone git@github.com:opendatacube/datacube-core.git  $SANDPIT/datacube-core
 git clone git@github.com:opendatacube/datacube-stats.git  $SANDPIT/datacube-stats
 git clone git@github.com:GeoscienceAustralia/digitalearthau.git  $SANDPIT/digitalearthau
+git clone git@github.com:GeoscienceAustralia/wofs.git  $SANDPIT/wofs
+git clone git@github.com:GeoscienceAustralia/fc  $SANDPIT/fc
