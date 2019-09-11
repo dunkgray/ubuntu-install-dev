244711e562074ec6ce2d7d3d9e3c619cf717c1a1
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-11 07:56:49 -0400

    Update start_install.sh
    auto-fy

--- start_install.sh
+++ start_install.sh
@@ -8,7 +8,7 @@
 # sudo apt-get install git -y
 # ssh-keygen -t rsa -b 4096 -C "duncan.gray@ga.gov.au"
 # more ~/.ssh/id_rsa.pub
-#$ sudo apt-get install xclip
+#$ sudo apt-get install xclip -y
 # Downloads and installs xclip. If you don't have `apt-get`, you might need to use another installer (like `yum`)
 #$ xclip -sel clip < ~/.ssh/id_rsa.pub
 # Copies the contents of the id_rsa.pub file to your clipboard
