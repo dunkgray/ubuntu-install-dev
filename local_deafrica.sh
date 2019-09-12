2a54ee4ca45833343afb9cb9d5d4e8e6b6954a6d
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-12 03:13:02 +0000

    deleting and moving files

--- local_deafrica.sh
+++
@@ -1,5 +0,0 @@
-git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git
-git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git
-git clone git@github.com:digitalearthafrica/config.git deafrica_config
-git clone git@github.com:dunkgray/DEAfrica_scripts.git
-git clone git@bitbucket.org:geoscienceaustralia/datakube-apps.git
