5754ae3f3fd620e26ac79eb8b56642242ba10387
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-13 01:16:51 +0000

    another repo and bug fix

--- clone_dea_repos.sh
+++ clone_dea_repos.sh
@@ -6,7 +6,7 @@ git clone git@github.com:opendatacube/datacube-core.git $SANDPIT/datacube-core
 
 #
 #git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git  $SANDPIT/landsat-to-cog
-# git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git ~/WOFLs-orchestration
+# git clone --branch landsat_usgs_testing git@github.com:opendatacube/WOFLs-orchestration.git $SANDPIT/WOFLs-orchestration
 git clone git@github.com:GeoscienceAustralia/eo-datasets.git $SANDPIT/eo-datasets
 git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git  $SANDPIT/cube-in-a-box
 git clone git@github.com:digitalearthafrica/config.git $SANDPIT/config
@@ -17,3 +17,4 @@ git clone git@github.com:opendatacube/datacube-stats.git  $SANDPIT/datacube-stat
 git clone git@github.com:GeoscienceAustralia/digitalearthau.git  $SANDPIT/digitalearthau
 git clone git@github.com:GeoscienceAustralia/wofs.git  $SANDPIT/wofs
 git clone git@github.com:GeoscienceAustralia/fc  $SANDPIT/fc
+git clone git@github.com:dunkgray/odc_testing.git $SANDPIT/odc_testing
