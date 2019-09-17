383511047d052265a3f2124e507582f95d462f36
Duncan Gray <duncan.gray@ga.gov.au>
2019-09-16 21:53:08 -0400

    coment

--- pip_repos.sh
+++ pip_repos.sh
@@ -7,6 +7,7 @@ pip install --user -e $SANDPIT/datacube-core/
 # pyproject.toml doesn't work with the pip command
 rm $SANDPIT/eo-datasets/pyproject.toml
 pip install --user -e $SANDPIT/eo-datasets
+# git checkout -- pyproject.toml
 pip install --user -e $SANDPIT/datacube-alchemist
 pip install --user -e $SANDPIT/digitalearthau #haven't done
 pip install --user -e $SANDPIT/datacube-stats
