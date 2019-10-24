81502ca78ff3128e6fa3a55b13888a2078eed5d1
Duncan Gray <duncan.gray@ga.gov.au>
2019-10-24 13:30:14 +1100

    minor

--- bashed_pod.sh
+++ bashed_pod.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get -y install iputils-ping bind9-host postgresql-client
 apt-get -y install emacs
 pip3 install awscli
-pip install --extra-index-url="https://packages.dea.ga.gov.au" odc-apps-cloud odc-apps-dc-tools
+pip3 install --extra-index-url="https://packages.dea.ga.gov.au" odc-apps-cloud odc-apps-dc-tools
