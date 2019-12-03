94a00807c3aa969eb73fe39b0f1ff5f734e76eec
Duncan Gray <duncan.gray@ga.gov.au>
2019-12-03 01:01:38 +0000

    update

--- clone_dea_repos.sh
+++ clone_dea_repos.sh
@@ -10,7 +10,7 @@ git clone git@github.com:opendatacube/datacube-core.git $SANDPIT/datacube-core
 git clone git@github.com:GeoscienceAustralia/eo-datasets.git $SANDPIT/eo-datasets
 git clone --branch usgs_l2_testing git@github.com:opendatacube/cube-in-a-box.git  $SANDPIT/cube-in-a-box
 git clone git@github.com:digitalearthafrica/config.git $SANDPIT/config
-git clone git@github.com:digitalearthafrica/DEAfrica_scripts.git $SANDPIT/DEAfrica_scripts
+git clone git@github.com:digitalearthafrica/scripts.git $SANDPIT/scripts
 git clone --branch africa git@github.com:opendatacube/datacube-alchemist.git  $SANDPIT/datacube-alchemist
 git clone git@github.com:opendatacube/datacube-core.git  $SANDPIT/datacube-core
 git clone git@github.com:opendatacube/datacube-stats.git  $SANDPIT/datacube-stats
@@ -19,3 +19,4 @@ git clone git@github.com:GeoscienceAustralia/wofs.git  $SANDPIT/wofs
 git clone git@github.com:GeoscienceAustralia/fc  $SANDPIT/fc
 git clone git@github.com:dunkgray/processingDEA.git $SANDPIT/processingDEA
 git clone git@github.com:GeoscienceAustralia/landsat-to-cog.git  $SANDPIT/landsat-to-cog
+git clone git@github.com:GeoscienceAustralia/dea-config.git  $SANDPIT/dea-config
