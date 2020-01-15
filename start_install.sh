9f5b747a6d203ae1d908557dd5ae03708553ce64
Duncan Gray <duncan.gray@ga.gov.au>
2020-01-15 05:07:15 +0000

    update

--- start_install.sh
+++ start_install.sh
@@ -32,6 +32,7 @@ sudo apt  install tree -y
 sudo apt  install screen -y
 sudo apt install python-pytest -y
 sudo apt install dos2unix -y
+sudo apt install pep8 -y
 
 pip install --extra-index-url="https://packages.dea.gadevs.ga" odc-apps-dc-tools odc-apps-cloud
 
