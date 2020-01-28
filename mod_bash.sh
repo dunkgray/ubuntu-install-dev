88352bc2b8edbf580a5738fd88df587d3e06a6fb
Duncan Gray <duncan.gray@ga.gov.au>
2020-01-29 09:21:45 +1100

    This seemed broken a while back...

--- mod_bash.sh
+++ mod_bash.sh
@@ -1,13 +1,14 @@
 echo "# ubuntu-install-dev mod_bash changes start " >> ~/.bashrc
 
-tee -a ~/.bashrc <<'END'
+# Likely broken
+# tee -a ~/.bashrc <<'END'
 
-# Change aws accounts and clusters
-ap(){
-export AWS_PROFILE= $1
-aws eks update-kubeconfig --name $1
-}
-END
+# # Change aws accounts and clusters
+# ap(){
+# export AWS_PROFILE=$1
+# aws eks update-kubeconfig --name $1
+# }
+# END
 
 echo "conda activate dea " >> ~/.bashrc
 echo "echo 'conda deactivate dea  # activate'" >> ~/.bashrc
