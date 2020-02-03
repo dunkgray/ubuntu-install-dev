f75aba86a73358b3cd1359aacdc9e90307edf55d
Duncan Gray <duncan.gray@ga.gov.au>
2020-02-03 06:01:36 +0000

    notes update

--- hyper_ub.sh
+++ hyper_ub.sh
@@ -1,7 +1,22 @@
-tee ~/etc/sysconfig/tvncservers <<'END'
-VNCSERVERS="1:<myusername-here>"
+# sudo ./hyper_ub.sh to run this 
+# Follow
+# https://docs.dev.dea.ga.gov.au/getting_started/hyper-v.html
+# to install TurboVNC
+
+# Download the latest .deb from the TurboVNC Downloads Page.
+# 2.2.4 didn't work for me
+# https://sourceforge.net/projects/turbovnc/files/
+# https://sourceforge.net/projects/turbovnc/files/2.2.3/
+# turbovnc_2.2.3_amd64.deb
+# wget https://sourceforge.net/projects/turbovnc/files/2.2.3/turbovnc_2.2.3_amd64.deb
+# sudo dpkg -i ~/Downloads/turbovnc*.deb
+
+
+
+tee /etc/sysconfig/tvncservers <<'END'
+VNCSERVERS="1:osboxes"
 END
 update-rc.d tvncserver defaults
 systemctl start tvncserver
-systemctl status tvncserve
+systemctl status tvncserver
 
