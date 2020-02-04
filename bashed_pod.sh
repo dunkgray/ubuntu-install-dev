d7da3c9f40dff76161afda6b36bb3df42a83e425
Duncan Gray <duncan.gray@ga.gov.au>
2020-02-04 14:36:15 +1100

    bashed pod goodness

--- bashed_pod.sh
+++ bashed_pod.sh
@@ -3,5 +3,6 @@
 apt-get update
 apt-get -y install iputils-ping bind9-host postgresql-client
 apt-get -y install emacs
+apt-get -y install nano
 pip3 install awscli
 pip3 install --extra-index-url="https://packages.dea.ga.gov.au" odc-apps-cloud odc-apps-dc-tools
