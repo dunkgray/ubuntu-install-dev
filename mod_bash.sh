f5972414ce0a6a3586f3aaff39660cbef4202956
Duncan Gray <duncan.gray@ga.gov.au>
2020-02-19 00:44:43 +0000

    comments

--- mod_bash.sh
+++ mod_bash.sh
@@ -2,7 +2,10 @@
 # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 # to
 # PS1="${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"
-# on the EC2 instance, so I cna tell local and aws apart.
+# on the EC2 instance, so I can tell local and aws apart.
+#
+# Consider incorporating some things from here;
+# https://docs.dev.dea.ga.gov.au/getting_started/workstation.html#setup-kubernetes-dev-cluster-access
 
 echo "# ubuntu-install-dev mod_bash changes start " >> ~/.bashrc
 
