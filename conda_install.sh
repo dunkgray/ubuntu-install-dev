a49e3b83dcc2d74fcfc577688e45315a4d927036
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-25 14:44:20 +1000

    installing a few packages

--- conda_install.sh
+++ conda_install.sh
@@ -28,7 +28,7 @@ conda create -c conda-forge --name dea -y python=3.6 psycopg2 click gdal jupyter
     singledispatch sqlalchemy structlog scipy \
     cattrs ciso8601 h5py pyproj ruamel.yaml shapely scikit-image \
     deepdiff flake8 pep8-naming python-rapidjson rio-cogeo numexpr requests \
-    pandoc pytest pytest-cov black ephem airflow
+    pandoc pytest pytest-cov black ephem airflow pycodestyle pylint geopandas
 # conda install -c conda-forge airflow
 # conda create -c conda-forge airflow
 
