24e5aaa4ab9ba6816ae753ba58e838bdf70711fb
Duncan Gray <duncan.gray@ga.gov.au>
2021-05-24 12:31:22 +1000

    30 day datssets, so data drops off.

--- aws_tests/standup.sh
+++ aws_tests/standup.sh
@@ -9,5 +9,14 @@ sudo yum -y install postgresql postgresql-devel python-devel
 sudo yum -y install gcc
 sudo yum -y install python3-devel
 pip install eodatasets3==0.18
-aws s3 cp s3://ga-sentinel/L1/LANDSAT/USGS-C2/2021-04-22/LC81010712021112LGN00/ $HOME/data/LC81010712021112LGN00/ --no-sign-request --recursive
-eo3-prepare landsat-l1 ~/data/LC81010712021112LGN00/LC08_L1TP_101071_20210422_20210422_02_RT_MTL.txt
+# This dataset just dropped off
+#aws s3 cp s3://ga-sentinel/L1/LANDSAT/USGS-C2/2021-04-22/LC81010712021112LGN00/ $HOME/data/LC81010712021112LGN00/ --no-sign-request --recursive
+
+#A new test data set;
+aws s3 cp s3://ga-sentinel/L1/LANDSAT/USGS-C2/2021-05-22/LC81030632021142LGN00/ $HOME/data/LC81030632021142LGN00/ --no-sign-request --recursive
+
+# dropped off
+#eo3-prepare landsat-l1 ~/data/LC81010712021112LGN00/LC08_L1TP_101071_20210422_20210422_02_RT_MTL.txt
+
+# This works!
+eo3-prepare landsat-l1 ~/data/*/*MTL.txt
