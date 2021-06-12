98d871c4bfa4fc358524f7c47d5250e197eb42ab
Duncan Gray <duncan.gray@ga.gov.au>
2021-06-12 00:53:10 -0400

    clean up

--- start_install.sh
+++ start_install.sh
@@ -23,8 +23,6 @@
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository universe
 
-
-
 sudo apt-get update
 sudo apt install awscli -y
 sudo apt-get install git -y
@@ -35,49 +33,39 @@ sudo apt-get install maven -y
 sudo apt install xterm -y
 sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y
 sudo apt-get install gfortran -y
-sudo apt  install tree -y
-sudo apt  install screen -y
+sudo apt install tree -y
+sudo apt install screen -y
 sudo apt install python-pytest -y
 sudo apt install dos2unix -y
 sudo apt install pep8 -y
-sudo apt install python-pip -y
 sudo apt install pylint -y
 sudo apt install zerofree -y
-sudo apt-get install libpq-dev
-sudo apt-get install ntp
+sudo apt-get install libpq-dev -y
+sudo apt-get install ntp -y
 sudo apt-get install curl -y
 
-pip3 install --extra-index-url="https://packages.dea.gadevs.ga" odc-apps-dc-tools odc-apps-cloud
-
-# Install terraform
-sudo git clone https://github.com/robertpeteuil/terraform-installer.git $HOME/terraform-installer
-$HOME/terraform-installer/terraform-install.sh -a
+#pip3 install --extra-index-url="https://packages.dea.gadevs.ga" odc-apps-dc-tools odc-apps-cloud
 
 # Install Pycharm
 #sudo snap install pycharm-community --classic
 sudo snap install pycharm-professional --classic
-
 
 #setup Xauthority
 mv .Xauthority old.Xauthority
 touch ~/.Xauthority
 xauth generate :0 . trusted
 
-# For dev
-mkdir pycharmremote
-
 # git setup
 git config --global user.name "Duncan Gray"
 git config --global user.email "duncan.gray@ga.gov.au"
 
-# set up the datbase
-sudo apt-get install postgresql postgresql-contrib -y
-
-
 sudo apt install python3-pip -y
 pip3 install awscli --upgrade --user
 
-# for wofs - should really move to conda/ check requirements.txt in wofs
-#pip3 install ephem
+# set up the datbase
+#sudo apt-get install postgresql postgresql-contrib -y
 
+# Install terraform
+#sudo git clone https://github.com/robertpeteuil/terraform-installer.git $HOME/terraform-installer
+#$HOME/terraform-installer/terraform-install.sh -a
 
